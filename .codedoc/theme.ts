5a0df236fdcaa5a9f4c7881c4450f8196e8c75a7
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-07-16 21:57:02 +0200

    fix issue with theme field name

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -36,12 +36,12 @@ export const theme = /*#__PURE__*/createTheme({
     light: {
       shadow: '',
       background: '#202040',
-      lineHightlight: '#363062'
+      lineHighlight: '#363062'
     },
     dark: {
       shadow: '',
       background: '#202040',
-      lineHightlight: '#363062'
+      lineHighlight: '#363062'
     }
   }
 });
